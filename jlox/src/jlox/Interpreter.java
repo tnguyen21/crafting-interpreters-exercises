6d8b2f3a6b6d32c0f3a16c7a3eb0df645a22cb16
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2022-09-30 12:10:09 -0500

    add conditional branching, loops, logical ops
    - if and else statements
    - short circuiting logical ops (and, or)
    - while loops
    - for loops by "desugaring" for-loop syntax into while loops
    
    ch 9 of crafting interpreters
    
    wow, this was a really cool chapter. very little code
    to introduce critical features in any modern programming
    language

--- jlox/src/jlox/Interpreter.java
+++ jlox/src/jlox/Interpreter.java
@@ -48,6 +48,16 @@ class Interpreter implements Expr.Visitor<Object>,
     }
 
     @Override
+    public Void visitIfStmt(Stmt.If stmt) {
+        if (isTruthy(evaluate(stmt.condition))) {
+            execute(stmt.thenBranch);
+        } else if (stmt.elseBranch != null) {
+            execute(stmt.elseBranch);
+        }
+        return null;
+    }
+
+    @Override
     public Void visitPrintStmt(Stmt.Print stmt) {
         Object value = evaluate(stmt.expression);
         System.out.println(stringify(value));
@@ -62,6 +72,14 @@ class Interpreter implements Expr.Visitor<Object>,
         }
 
         environment.define(stmt.name.lexeme, value);
+        return null;
+    }
+
+    @Override
+    public Void visitWhileStmt(Stmt.While stmt) {
+        while (isTruthy(evaluate(stmt.condition))) {
+            execute(stmt.body);
+        }
         return null;
     }
 
@@ -121,6 +139,19 @@ class Interpreter implements Expr.Visitor<Object>,
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
         return expr.value;
+    }
+
+    @Override
+    public Object visitLogicalExpr(Expr.Logical expr) {
+        Object left = evaluate(expr.left);
+
+        if (expr.operator.type == TokenType.OR) {
+            if (isTruthy(left)) return left;
+        } else {
+            if (!isTruthy(left)) return left;
+        }
+
+        return evaluate(expr.right);
     }
 
     @Override
