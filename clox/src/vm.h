3061283eb41c3f70779ddf5a38037189df548ae5
Tommy Bui Nguyen <tommynguyen0512@gmail.com>
2022-11-25 20:34:18 -0600

    add string type to lang and scaffold for GC
    GC = garbage collection

--- clox/src/vm.h
+++ clox/src/vm.h
@@ -4,11 +4,13 @@
 #include "chunk.h"
 #include "value.h"
 #define STACK_MAX 256
+
 typedef struct {
     Chunk* chunk;
     uint8_t* ip;
     Value stack[STACK_MAX];
     Value* stackTop;
+    Obj* objects;
 } VM;
 
 typedef enum {
@@ -16,6 +18,8 @@ typedef enum {
     INTERPRET_COMPILE_ERROR,
     INTERPRET_RUNTIME_ERROR
 } InterpretResult;
+
+extern VM vm;
 
 void initVM();
 void freeVM();
