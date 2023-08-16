269f6a2dca8004ebb23f923bbd63e89512ffac9f
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2023-08-16 09:24:30 -0500

    a little code golf

--- pylox/src/my_token.py
+++ pylox/src/my_token.py
@@ -1,13 +1,8 @@
 from typing import Any
 from token_type import TokenType
 
-
 class Token:
     def __init__(self, type: TokenType, lexeme: str, literal: Any, line: int):
-        self.type = type
-        self.lexeme = lexeme
-        self.literal = literal
-        self.line = line
+        self.type,self.lexeme,self.literal,self.line = type,lexeme,literal,line
 
-    def __str__(self):
-        return f"{self.type} {self.lexeme} {self.literal}"
+    def __str__(self): return f"{self.type} {self.lexeme} {self.literal}"
